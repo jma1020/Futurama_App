d54c9e91df136cf0fa1fff4a5a9daef8340210b5
jeongmin Kim <jma1020@naver.com>
2021-12-28 03:42:00 +0900

    💄 Episode페이지 스타일 추가

--- src/pages/episodes/index.tsx
+++ src/pages/episodes/index.tsx
@@ -3,6 +3,7 @@ import useSWR from "swr";
 import { fetcher } from "../../utils/fetcher";
 import { API_ENDPOINT } from "../../constants";
 import { EpisodeData } from "../../types/episodeData";
+import styled from "@emotion/styled";
 
 const EpsiodePage:NextPage = () =>{
     
@@ -15,21 +16,44 @@ const EpsiodePage:NextPage = () =>{
     
 
     return(
-        <div>
-            난에피
+        <EpiBox>
+   
             {data.map((item:EpisodeData)=>{
                 const {desc, id, number, originalAirDate, title, writers} = item
                 return(
-                    <div key={`episode-${id}`}>
-                        <h1>{number}  {title}</h1>
-                        <p>{desc}</p>
+                    <EpiCard key={`episode-${id}`}>
+                        <h1>({number})  {title}</h1>
                         <p>{originalAirDate}</p>
                         <small>{writers}</small>
-                    </div>
+                        <p>{desc}</p>
+                        
+                       
+                    </EpiCard>
                 )
             })}
-        </div>
+        </EpiBox>
     )
 }
+
+const EpiBox = styled.div`
+    display: grid;
+    grid-template-columns:repeat(auto-fill, 300px) ;
+    grid-auto-rows: 400px;
+    gap: 50px;
+    padding: 50px;
+    text-align: center;
+    
+`
+const EpiCard = styled.div`
+    background-color: white;
+    border-radius: 20px;
+    box-shadow: 5px 5px 5px 5px gray;
+
+    &:hover{
+        background-color: #ffbf8a;
+    }
+`
+
+
 
 export default EpsiodePage
