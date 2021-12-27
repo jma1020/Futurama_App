af831a21ada4afb7d1bed4500f8d0f7be98fb952
jeongmin Kim <jma1020@naver.com>
2021-12-28 03:19:04 +0900

    💄 CharacterDetail 스타일 추가

--- src/pages/characters/[id].tsx
+++ src/pages/characters/[id].tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useCharacterData } from "../../hooks/useCharacterData";
@@ -10,19 +11,35 @@ const CharacterDetail: NextPage = () =>{
 
     if (error) return <div>"An error has occurred.";</div>
     if (!data) return <div>Loadings</div>
-
-    const { images, name,sayings} =data
+    console.log(data)
+    const { images, name, sayings, age, homePlanet, species} =data
   
-    return(
-        <div>
-            <img src={images.main} alt="" />
-            <h2>{name.first} {name.last}</h2>
-            {sayings.map((saying: string, index: number) => {
-                return <p key={`saying-${index}`}>{saying}</p>
-            })}
+    
 
-        </div>
+    const randomNumber =Math.floor(Math.random() * sayings.length);
+
+    return(
+        <CardDetail>
+            <img src={images.main} alt="" />
+            <h2>{name.first} {name.last} ({age})</h2>
+            <p>planet : {homePlanet}, species : {species}</p>
+            <h3>{sayings[randomNumber]}</h3>
+            {/* {sayings.map((saying: string, index: number) => {            
+                return <p key={`saying-${index}`}>{saying}</p>
+            })} */}
+        </CardDetail>
     )
 }
 
+const CardDetail = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 50px;
+`
+
+
 export default CharacterDetail;
+
+
